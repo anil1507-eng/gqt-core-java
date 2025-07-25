700332c5ee07daad00b9b26c9354aa1dfc5b6d42
Anilkumar-2003 <anilkumarvagicharla32@gmail.com>
2025-07-25 09:23:20 +0530

    add programs(20-22)

---
+++ src/com/gqt/corejava/patterns/pattern22.java
@@ -0,0 +1,24 @@
+package com.gqt.corejava.patterns;
+
+import java.util.Scanner;
+
+public class pattern22 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("enter the size:");
+		int n = sc.nextInt();
+		
+		for( int i =n; i>=1;i--) {
+			
+			for( int j=n; j>=i;j--) {
+				System.out.print("- ");
+			}
+			for( int j =1;j<=i;j++) {
+				System.out.print("# ");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
