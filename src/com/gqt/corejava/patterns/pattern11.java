e666c1829535d212e0ac19bc6f5fe0fc8bad09c3
Anilkumar-2003 <anilkumarvagicharla32@gmail.com>
2025-07-25 09:06:51 +0530

    loaded all the pattern programs

---
+++ src/com/gqt/corejava/patterns/pattern11.java
@@ -0,0 +1,26 @@
+package com.gqt.corejava.patterns;
+
+import java.util.Scanner;
+
+public class pattern11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the count:");
+		int n = sc.nextInt();
+		
+		int m =1;
+		
+		for ( int i =1; i<=n; i++) {
+			for ( int j =1; j <=n ; j++) {
+				System.out.print(m+" ");
+				m++;
+			}
+			System.out.println(" ");
+		}
+		sc.close();
+	}
+
+}
---
+++ src/com/gqt/corejava/patterns/pattern19.java
@@ -0,0 +1,24 @@
+package com.gqt.corejava.patterns;
+
+import java.util.Scanner;
+
+public class pattern19 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the size: ");
+		int n = sc.nextInt();
+		int m =1;
+	
+		for( int i =n; i>=1; i--) {
+			for( int j =1; j<=i; j++) {
+				System.out.print(m+" ");
+				m++;
+			
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
